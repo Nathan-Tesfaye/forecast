f0c8edbb980c1c54d9b295e8b8f26710970957b0
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2025-01-11 14:56:18 +0300

    done CurrentCard.jsx no style

---
+++ src/components/CurrentCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import humidity from '../assets/humidity.png'
+import wind from '../assets/wind.png'
+
+const CurrentCard = ({weatherData}) => {
+
+  if(!weatherData){
+    return null;
+  }
+
+  console.log(weatherData)
+  return (
+    <div>
+      <img src={weatherData.icon} alt="condition icon" />
+      <p>{weatherData.temp}&deg;C</p>
+      <p>{weatherData.location}</p>
+      <div>
+        <img src={humidity} alt="" />
+        <p>{weatherData.humidity}</p>
+      </div>
+      <div>
+        <img src={wind} alt="" />
+        <p>{weatherData.windSpeed} km/hr</p>
+      </div>
+    </div>
+  )
+}
+
+export default CurrentCard
---
+++ src/components/Weather.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import CurrentCard from "./CurrentCard";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState([]);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    if (city === "") {
+      alert("please enter city name.");
+      return;
+    }
+
+    try {
+      //fetch current weather data
+      const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const currentWeatherResponse = await fetch(currentWeatherUrl);
+      const currentWeatherData = await currentWeatherResponse.json();
+
+      if (!currentWeatherResponse.ok) {
+        alert(currentWeatherData.message);
+        return;
+      }
+
+      // console.log(currentWeatherData);
+      const icon = allIcons[currentWeatherData.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: currentWeatherData.main.humidity,
+        windSpeed: currentWeatherData.wind.speed,
+        temp: Math.floor(currentWeatherData.main.temp),
+        location: currentWeatherData.name,
+        icon: icon,
+      });
+
+      //Fetch forecast data
+      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const forecastResponse = await fetch(forecastUrl);
+      const futureData = await forecastResponse.json();
+
+      if (!forecastResponse.ok) {
+        alert(futureData.message);
+        return;
+      }
+
+      // Filter for the next 5 3-hourly forecasts
+      const nextFiveForecast = futureData.list.slice(0, 5).map((i) => ({
+        time: i.dt_txt,
+        temperature: Math.floor(i.main.temp),
+        condition: i.weather[0].description,
+        icon: allIcons[i.weather[0].icon] || clear_icon,
+      }));
+
+      // console.log(futureData);
+      setForecastData(nextFiveForecast);
+    } catch (error) {
+      console.error("Can't fetch data.");
+    }
+  };
+
+  useEffect(() => {
+    search("Addis Ababa");
+  }, []);
+  return <div>
+   <CurrentCard weatherData = {weatherData} />
+  </div>;
+};
+
+export default Weather;
